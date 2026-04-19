701a6ed00fd59ba100d177168f192de9c219d43c
its-YogeshChandra <pandittheroyal@gmail.com>
2026-04-19 20:52:15 +0530

    initializing eventide

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+//create http wrapper of our own
+use tokio::net::TcpListener;
+
+// work would be done in layers
+
+//bind tcp listener to the address
+
+fn main() {
+    let address = "127.0.0.1:8080";
+    println!("Hello, world!");
+}
